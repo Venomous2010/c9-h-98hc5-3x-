8573ed77ed9e69501415aed8d772a8ebf316a6a3
Venomous2010 <79148708+Venomous2010@users.noreply.github.com>
2021-11-14 13:20:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+miliSeconds = 0;
+
+    miliSeconds++;
+
+    if(miliSeconds = 300){
+        miliSeconds = 0;
+        miliSeconds++;
+    }
+
+document.getElementById("timer").innerHTML = "timer" + miliSeconds; 
